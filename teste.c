dd865df2f518ee744736bc9a4dabee5e3fb50bdb
Bruno-LS <111468518+Bruno-LS@users.noreply.github.com>
2022-09-22 08:43:57 -0300

    Update teste.c

--- teste.c
+++ teste.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#define TAM 20
+/*#define TAM 20*/
 
 //Ano
 typedef struct{
@@ -22,27 +22,88 @@ typedef struct {
 
 
 
-void buscar_carro(T_carro agencia[TAM], int quant, char m[], int a1, int a2, float x);
+
+void buscar_carro(T_carro agencia[], int quant, char m[], int a1, int a2, float x);
+
 void exibir_dados_carro(T_carro carro);
+
 void reajustar_valor(T_carro agencia[],int quant);
+
+int remover(T_carro agencia[], int *quant, char placa[]);
+
 
 void  main()
 {
-	int a1, a2;
+	int a1, a2, TAM=20, i;
 	float x;
-	char modelo[20]; 
-	
+	char modelo[20], placa[9], op; 	
 	T_carro agencia[TAM];
 	
-	buscar_carro(agencia, TAM, modelo, a1, a2, x);
-	reajustar_valor(agencia,TAM);
+	for(i=0;i<TAM;i++)
+	{
 		
+		printf("Placa: \n");
+		fflush(stdin);
+		gets(agencia[i].placa);
+	
+		printf("Marca/Modelo: \n");
+		fflush(stdin);
+		gets(agencia[i].modelo);
+		
+		printf("Cor: \n");
+		fflush(stdin);
+		gets(agencia[i].cor);
+		
+		printf("quilometragem: \n");
+		scanf("%f", &agencia[i].km);
+		
+		printf("ANO de fabricacao: \n");	
+		scanf("%d", &agencia[i].ano.fabricacao);
+		
+		printf("ANO do modelo: \n");
+		scanf("%d", &agencia[i].ano.modelo);
+		
+		printf("Valor:R$ \n");
+		scanf("%f", &agencia[i].valor);
+		
+		printf("Tipo: \n");
+		fflush(stdin);
+		gets(agencia[i].tipo);
+		
+		printf("Marca: \n\n");
+		fflush(stdin);
+		gets(agencia[i].marca);
+	}
 
+	reajustar_valor(agencia,TAM);
+	
+	//pegando opção
+	scanf("%c", &op);
+	
+	switch(op)
+	{
+		case 'b':
+			//pegando modelo
+			fflush(stdin);
+			gets(modelo);
+			//pegando primeiro ano
+			scanf("%d", &a1);
+			//pegando segundo ano
+			scanf("%d", &a2);
+			//pegando valor
+			scanf("%f", &x);
+			buscar_carro(agencia, TAM, modelo, a1, a2, x);
+	
+		case 'r':
+			fflush(stdin);
+			gets(placa);
+			remover(agencia, &TAM, placa);			
+	}
 	
 }
 
 
-void buscar_carro(T_carro agencia[TAM], int quant, char m[], int a1, int a2, float x)
+void buscar_carro(T_carro agencia[], int quant, char m[], int a1, int a2, float x)
 {
 	int i, cont=0;
 	
@@ -78,6 +139,8 @@ void exibir_dados_carro(T_carro carro)
 		
 	
 }
+
+
 void reajustar_valor(T_carro agencia[],int quant)
 {
 	int i;
@@ -89,6 +152,28 @@ void reajustar_valor(T_carro agencia[],int quant)
 			agencia[i].valor *=1.025;
 		}
 	}
+}
+
+
+int remover(T_carro agencia[], int *quant, char placa[])
+{
+	int i, j;
+	
+	
+	for(i=0;i<*quant;i++)
+	{
+		if(strcmp(placa, agencia[i].placa)==0)
+		{
+			for(j=i;j<*quant-1;j++)
+			{
+				agencia[j] = agencia[j+1];
+				
+			}
+		}
+	}
+	*quant--;
+	return 1;
+	
 }
 
 
